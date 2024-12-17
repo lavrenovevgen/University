305c5067310fc3f8a76dc49f317150c59592fe19
lavrenovevgen <lavrenovevgen80@gmail.com>
2024-12-17 14:43:16 +0500

    Add files via upload

--- ДЗ_переменные.py
+++ ДЗ_переменные.py
@@ -2,4 +2,4 @@ completed_DZ=12
 hours_spent=1.5
 course='Python'
 time_for_task=hours_spent/completed_DZ
-print("Курс: "+course+ ", всего задач:"+str(completed_DZ)+", затрачено часов: "+str(hours_spent)+", среднее время выполнения: "+str(time_for_task)+" часа.")
+print("Курс: "+course+ ", всего задач: "+str(completed_DZ)+", затрачено часов: "+str(hours_spent)+", среднее время выполнения: "+str(time_for_task)+" часа.")
